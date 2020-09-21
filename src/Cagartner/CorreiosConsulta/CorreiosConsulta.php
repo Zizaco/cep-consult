d35d8d75743a70906ab117232d0a34c8d611fd5a
Carlos Augusto Gartner <contato@carlosgartner.com.br>
2020-09-21 11:08:55 -0300

    PHP4 support fixes

--- src/Cagartner/CorreiosConsulta/CorreiosConsulta.php
+++ src/Cagartner/CorreiosConsulta/CorreiosConsulta.php
@@ -129,7 +129,7 @@ class CorreiosConsulta
                     'valor_declarado'    => self::cleanMoney($rate->ValorValorDeclarado),
                     'entrega_domiciliar' => $rate->EntregaDomiciliar === 'S',
                     'entrega_sabado'     => $rate->EntregaSabado === 'S',
-                    'erro'               => ['codigo' => (real)$rate->Erro, 'mensagem' => (real)$rate->MsgErro],
+                    'erro'               => ['codigo' => (float)$rate->Erro, 'mensagem' => $rate->MsgErro],
                 ];
 
             }
@@ -223,7 +223,7 @@ class CorreiosConsulta
                 [$data, $hora, $local] = explode("<br>", phpQuery::pq($tr)->find('td:eq(0)')->html());
                 [$status, $encaminhado] = explode("<br>", phpQuery::pq($tr)->find('td:eq(1)')->html());
 
-                $rastreamento[] = ['data' => trim($data) . " " . trim($hora), 'local' => trim($local), 'status' => trim(strip_tags($status))];
+                $rastreamento[] = array('data' => strip_tags(trim($data)) . " " . strip_tags(trim($hora)), 'local' => strip_tags(trim($local)), 'status' => strip_tags(trim($status)));
 
                 if (trim($encaminhado)) {
                     $rastreamento[count($rastreamento) - 1]['encaminhado'] = trim($encaminhado);
